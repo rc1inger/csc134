dfa6e27b976dd493922c39a4ca626fdeeda29fb6
rc1inger <rclinger@csus.edu>
2023-04-27 16:26:20 -0700

    Override executeQuery()
    Started over-riding executeQuery with a String in the method signature. This is for repeated use of showing the result set.

--- Driver.java
+++ Driver.java
@@ -7,9 +7,12 @@ class Driver {
     // take care of input and input buffer here
         DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
         Connection con = DriverManager.getConnection(
-            "jdbc:oracle:thin:@localhost:1521:CSC134", "rickc", "Password123");
-
-        setupDB(con);
+        //    "jdbc:oracle:thin:@sabzevi2.homeip.net:1521:orcl", "csus", "student");
+            "jdbc:oracle:thin:@sabzevi2.homeip.net:1521:orcl", "csus", "student");
+        Statement st = con.createStatement();
+        try
+        {
+        setupDB(st);
         // Menu Array
         String[] menu = {
             "Insert",
@@ -26,101 +29,108 @@ class Driver {
         System.out.println();
         System.out.print("Choose#: ");
         Scanner scanner = new Scanner(System.in);
-        switch (scanner.nextInt()) {
+        
+        String inputString = scanner.nextLine();
+        int choice = Integer.parseInt(inputString);
+        
+        switch (choice) {
             case 1:
-               String userInput = scanner.nextLine();
                System.out.println(); 
                System.out.print("Insert Menu: \nFrom which table do you want to insert?");
-               showTables(con);
+               showTables(st);
                System.out.print("Type table_name: ");
-               userInput = scanner.nextLine();
+               inputString = scanner.nextLine();
 
-               if (userInput.equalsIgnoreCase("team")) {
-                   showTeams(con);
+               if (inputString.equalsIgnoreCase("team")) {
+                   showTeams(st);
+                   System.out.println("This executed");
                }
 
-               if (userInput.equalsIgnoreCase("player")) {
-                   showPlayers(con);
+               if (inputString.equalsIgnoreCase("player")) {
+                   showPlayers(st);
                }
                break;
         }
-        dropTables(con);
+            dropTables(st); // exit code 0
+        }
+        catch (SQLException e)
+        {   
+            e.printStackTrace();
+            dropTables(st); // exit code 1
+        }
     } // end main
 
 
     // Setup db
-    public static void setupDB(Connection con) throws Exception{
-        Statement st = con.createStatement();
+    public static void setupDB(Statement st) throws Exception{
         st.executeQuery("create table ling_team (teamid number NOT NULL, teamname varchar2(30) NOT NULL, CONSTRAINT pk_team PRIMARY KEY(teamid))");
         st.executeQuery("create table ling_player (playerid number NOT NULL, fname varchar2(20) NOT NULL, lname varchar2(20) NOT NULL, position varchar2(20), captainrole varchar2(1), dob date, salary number, teamid number NOT NULL, CONSTRAINT pk_player PRIMARY KEY(playerid, teamid), CONSTRAINT fk_team FOREIGN KEY (teamid) REFERENCES ling_team(teamid))");
         st.executeQuery("insert into ling_team values (100, 'Sharks')");
         st.executeQuery("insert into ling_player values (39, 'Logan', 'Couture', 'Center', 'C', to_date('1989-03-28', 'YYYY-MM-DD'), 2000000, 100)");
     }
-    // Show tables
-    public static void showTables(Connection con) throws Exception{
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select substr(table_name,6) from user_tables where lower(table_name) like 'ling%'");
-        System.out.println("TABLE_NAME");
+    // Execute query
+    public static void executeQuery(Statement st,String query) throws Exception{
+        ResultSet rs = st.executeQuery(query);
 
         while (rs.next()) {
             int numColumns = rs.getMetaData().getColumnCount();
 
             for (int i = 1; i <= numColumns; i++) {
                 String value = rs.getString(i);
-
                 if (value != null) {
                     System.out.print("| " + value + "\t");
                 }
             }
 
-            System.out.print("\n");
+            System.out.println();
         }
-        // newline and clear buffer
-        System.out.println("\n");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+    }
+    
+    
+    
+    
+    
+    // Show tables
+    public static void showTables(Statement st) throws Exception{
+        System.out.println("TABLE_NAME");
+        executeQuery(st, "select substr(table_name,6) from user_tables where lower(table_name) like 'ling%'");
     }
     
     // Drop tables
-    public static void dropTables(Connection con) throws Exception
+    public static void dropTables(Statement st) throws Exception
     {
-        Statement st =con.createStatement();
         st.executeQuery("drop table ling_player cascade constraints");
         st.executeQuery("drop table ling_team cascade constraints");
+        System.out.println("dropped tables successfully");
     }
     
-    // Show Players
-    public static void showPlayers(Connection con) throws Exception
+    public static void showPlayers(Statement st) throws Exception
     {
-        Statement st = con.createStatement();
-        st.executeQuery("select * from ling_player");
+        executeQuery(st, "select * from ling_player");
     }
-    
-    // Show Team
-   public static void showTeams(Connection con) throws Exception
+   public static void showTeams(Statement st) throws Exception
     {
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from user_tables");
-        
-        System.out.println("This is not working");
+        executeQuery(st, "select * from ling_team");
     }
     
     // Insert
-    public static void showInsertMenu(Connection con)
+    public static void showInsertMenu(Statement st)
     {
     }
     
     // Update
-    public static void showUpdateMenu(Connection con)
+    public static void showUpdateMenu(Statement st)
     {
     }
     
     // Delete
-    public static void showDeleteMenu(Connection con)
+    public static void showDeleteMenu(Statement st)
     {
     }
     
     // View
-    public static void showViewMenu(Connection con)
+    public static void showViewMenu(Statement st)
     {
     }
 
