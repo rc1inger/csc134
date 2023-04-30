15b5eb2b5de54e60b67e1681e6d6beff7566a7bd
rc1inger <rclinger@csus.edu>
2023-04-30 10:24:38 -0700

    Added View function.

--- Driver.java
+++ Driver.java
@@ -35,14 +35,17 @@ class Driver {
                 case 1:
                     insertMenu(st);
                     break;
-
                 case 2:
-                    System.out.println();
                     deletePlayer(st);
                     break;
+                case 3:
+                    updateTeam(st);
+                    break; 
+                case 4:
+                    viewData(st);
+                    break;
                 case 5:
-                    exit=true;    
-
+                    exit=true;
                 default:
                     break;
                 }
@@ -59,9 +62,42 @@ class Driver {
     // Setup db
     public static void setupDB(Statement st) throws Exception {
         st.executeQuery("create table ling_team (teamid number NOT NULL, teamname varchar2(30) NOT NULL, CONSTRAINT pk_team PRIMARY KEY(teamid))");
-        st.executeQuery("create table ling_player (playerid number NOT NULL, fname varchar2(20) NOT NULL, lname varchar2(20) NOT NULL, position varchar2(20), captainrole varchar2(1), dob date, salary number, teamid number NOT NULL, CONSTRAINT pk_player PRIMARY KEY(playerid, teamid), CONSTRAINT fk_player_team FOREIGN KEY (teamid) REFERENCES ling_team(teamid) ON DELETE CASCADE)");
+        st.executeQuery("create table ling_player (playerid number NOT NULL, fname varchar2(20) NOT NULL, lname varchar2(20) NOT NULL, pos varchar2(20), captainrole varchar2(1), dob date, salary number, teamid number NOT NULL, CONSTRAINT pk_player PRIMARY KEY(playerid, teamid), CONSTRAINT fk_player_team FOREIGN KEY (teamid) REFERENCES ling_team(teamid) ON DELETE CASCADE)");
         st.executeQuery("insert into ling_team values (100, 'Sharks')");
         st.executeQuery("insert into ling_player values (39, 'Logan', 'Couture', 'C', 'C', to_date('1989-03-28', 'YYYY-MM-DD'), 2000000, 100)");
+    }
+    
+    
+    // View data
+    public static void viewData(Statement st) throws Exception {
+        try{
+            executeQuery(st, "select t.teamname, (fname || ' ' || lname) as Name, pos, captainrole, trunc((months_between(sysdate, dob)/12)) as age, salary FROM ling_player p INNER JOIN ling_team t ON t.teamid=p.teamid"); 
+        
+        }
+        catch(Exception e) {
+        
+        }
+    }
+    
+    // Update team
+    public static void updateTeam(Statement st) {
+        try{
+           showTeams(st);
+           System.out.println("Case-sensitive");
+           System.out.print("Type the team name: ");
+           Scanner scanner = new Scanner(System.in);
+           String inputStringTeamName = scanner.nextLine();
+           System.out.print("What do you want to change it to?: ");
+           String inputStringNewTeamName = scanner.nextLine();
+           
+           st.executeQuery("update ling_team set teamname='"+inputStringNewTeamName+"' where teamname='"+inputStringTeamName+"'");
+           showTeams(st);
+           System.out.println();
+
+         }
+         catch (Exception e) {
+             e.printStackTrace();
+         }
     }
     
     // Delete player
@@ -77,7 +113,7 @@ class Driver {
                 System.out.println("input must not be empty!\n");
             } else if (!isDigit(inputStringTeamID)) {
                 System.out.println("input must be a number!\n");
-            } else if (!checkTeam(st, inputStringTeamID)) {
+            } else if (!checkTeamID(st, inputStringTeamID)) {
                 System.out.println("That team ID # does not exists!\n");
             } else if (isDigit(inputStringTeamID)) {
                 executeQuerySimple(st, "select teamname from ling_team where teamid="+inputStringTeamID);
@@ -151,7 +187,7 @@ class Driver {
                     System.out.println("input must not be empty!\n");
                 }else if (!isDigit(inputStringTeamID)) {
                     System.out.println("input must be a number!");
-                } else if (checkTeam(st, inputStringTeamID)) {
+                } else if (checkTeamID(st, inputStringTeamID)) {
                     System.out.println("That teamID # already exists!\n");
                     return;
                 } 
@@ -174,7 +210,7 @@ class Driver {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Type in the team ID # to insert player: ");
             String inputStringTeamID = scanner.nextLine();
-            if (!checkTeam(st, inputStringTeamID)) {
+            if (!checkTeamID(st, inputStringTeamID)) {
                 System.out.println("Team id # doesn't exist.");
                 System.out.println("Do you want to create a new team? (y/n): ");
                 String choice = scanner.nextLine();
@@ -183,7 +219,7 @@ class Driver {
                 else
                     insertTeam(st);
             }
-            if (checkTeam(st, inputStringTeamID)) {
+            if (checkTeamID(st, inputStringTeamID)) {
                 showTeamAndPlayers(st, inputStringTeamID);
                 System.out.print("Player number: ");
                 int playerID = Integer.parseInt(scanner.nextLine());
@@ -192,14 +228,14 @@ class Driver {
                 System.out.print("Last name: ");
                 String lname = scanner.nextLine();
                 System.out.print("Position(F/D/G): ");
-                String position = scanner.nextLine();
+                String pos = scanner.nextLine();
                 System.out.print("Captain(C/A): ");
                 String captainRole = scanner.nextLine();
                 System.out.print("Date of birth(MM-DD-YYYY): ");
                 String dob = scanner.nextLine();
                 System.out.print("Salary: ");
                 double salary = Double.parseDouble(scanner.nextLine());
-                addRow(st, "ling_player", playerID + ",'" + fname + "','" + lname + "','" + position + "','" + captainRole + "',to_date('" + dob + "','MM-DD-YYYY')," + salary + "," + inputStringTeamID);
+                addRow(st, "ling_player", playerID + ",'" + fname + "','" + lname + "','" + pos + "','" + captainRole + "',to_date('" + dob + "','MM-DD-YYYY')," + salary + "," + inputStringTeamID);
                 showTeamAndPlayers(st, inputStringTeamID);
             }
         } catch (Exception e) {
@@ -225,7 +261,7 @@ class Driver {
     }
 
     // Check team
-    public static boolean checkTeam(Statement st, String teamID) throws Exception {
+    public static boolean checkTeamID(Statement st, String teamID) throws Exception {
         try {
             ResultSet rs = st.executeQuery("select teamid from ling_team where teamid=" + teamID);
             rs.next();
